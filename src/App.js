961717d4fd616a3ca521b96ab001b64b1c23cff8
ahering <ahering@gmail.com>
2021-12-10 06:15:29 -0600

    Saving code changes

--- src/App.js
+++ src/App.js
@@ -1,58 +1,74 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter, Route, Routes, Link } from 'react-router-dom'
+import ReactDOM from 'react-dom'
 import Slider from './components/NetflixSlider'
+import axios from "axios"
 import './App.scss'
 
-const movies = [
-  {
-    id: 1,
-    image: '/images/slide1.jpg',
-    imageBg: '/images/slide1b.webp',
-    title: '1983'
-  },
-  {
-    id: 2,
-    image: '/images/slide2.jpg',
-    imageBg: '/images/slide2b.webp',
-    title: 'Russian doll'
-  },
-  {
-    id: 3,
-    image: '/images/slide3.jpg',
-    imageBg: '/images/slide3b.webp',
-    title: 'The rain',
-  },
-  {
-    id: 4,
-    image: '/images/slide4.jpg',
-    imageBg: '/images/slide4b.webp',
-    title: 'Sex education'
-  },
-  {
-    id: 5,
-    image: '/images/slide5.jpg',
-    imageBg: '/images/slide5b.webp',
-    title: 'Elite'
-  },
-  {
-    id: 6,
-    image: '/images/slide6.jpg',
-    imageBg: '/images/slide6b.webp',
-    title: 'Black mirror'
-  }
-];
-
-class App extends Component {
-  render() {
-    return (
-      <div className="app">
-        <Slider>
-          {movies.map(movie => (
-            <Slider.Item movie={movie} key={movie.id}>item1</Slider.Item>
-          ))}
-        </Slider>
+const PortalModal = () => {
+  return ReactDOM.createPortal((
+      <div>
+          Hello from Model
       </div>
-    );
-  }
+  ), document.getElementById('modal-root'))
 }
 
-export default App;
+function setToken(key,value) {
+  sessionStorage.setItem(key, JSON.stringify(value));
+}
+
+function getToken(key) {
+  const ssKey = sessionStorage.getItem(key);
+  const jKey = JSON.parse(ssKey);
+  return jKey
+}
+
+function allmovies() {
+  const [isLoading, setLoading] = useState(true);
+  const [movies, setItems] = useState();
+  useEffect(() => {
+      const API_PATH = 'http://local/movies.php';
+      axios.get(
+          `${API_PATH}`, { crossdomain: true }
+          ).then(response => {
+              setItems(response.data);
+              setLoading(false);
+          });
+    }, []);
+
+  if (isLoading && !!!movies) {
+      return <div className="app loading"><div>Cargando...</div></div>;
+  }
+
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/page">Page</Link></li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/page" element={<p>Hola Mundo</p>}>
+          </Route>
+          <Route exact path="/" 
+            element={<Slider>
+              {movies.map(movie => (
+                <Slider.Item movie={movie} key={movie.id}></Slider.Item>
+              ))}
+            </Slider>}>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+        <div className="footer">
+            <div>
+              <div>Martha Guzmán Fotografía</div>
+              <div>2021 Todos los derechos reservados</div>
+            </div>
+        </div>
+    </div>
+  );
+}
+
+export default allmovies;
